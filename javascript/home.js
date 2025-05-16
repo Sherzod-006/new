673bc80d1c20f5920c0fd2ee16ef70fd6ef2fbf2
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-05-16 22:29:09 +0500

    gained navbar details and tools

--- javascript/home.js
+++ javascript/home.js
@@ -1,10 +1,18 @@
 // This script toggles between light and dark mode for the webpage.
-document.getElementById("toggle-dark-mode").addEventListener("click", function() {
-    document.querySelectorAll(".light, .dark").forEach(element => {
-        if (element.classList.contains("light")) {
-            element.classList.replace("light", "dark");
-        } else {
-            element.classList.replace("dark", "light");
-        }
+document
+  .getElementById("toggle-dark-mode")
+  .addEventListener("click", function () {
+    document.querySelectorAll(".light, .dark").forEach((element) => {
+      if (element.classList.contains("light")) {
+        element.classList.replace("light", "dark");
+        document
+          .querySelector(".fa-moon")
+          .classList.replace("fa-moon", "fa-sun");
+      } else {
+        element.classList.replace("dark", "light");
+        document
+          .querySelector(".fa-sun")
+          .classList.replace("fa-sun", "fa-moon");
+      }
     });
-});
+  });
