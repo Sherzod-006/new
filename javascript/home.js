85e8712c2d770018fa165e5107b7d1c8b3450b0f
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-05-17 18:32:28 +0500

    updated settings navBar and complated header&navbar

--- javascript/home.js
+++ javascript/home.js
@@ -10,6 +10,8 @@ document
 
         document.querySelector(".white").classList.replace("white", "black");
         document.querySelector(".setbut").classList.add("black");
+        document.querySelector(".sigbut").classList.add("black");
+        document.querySelector(".logbut").classList.add("black");
       } else {
         element.classList.replace("dark", "light");
         document.querySelector(".fa-sun").classList.add("iact");
@@ -17,6 +19,8 @@ document
 
         document.querySelector(".black").classList.replace("black", "white");
         document.querySelector(".setbut").classList.remove("black");
+        document.querySelector(".sigbut").classList.remove("black");
+        document.querySelector(".logbut").classList.remove("black");
       }
     });
   });
@@ -28,6 +32,28 @@ document.getElementById("setbutt").addEventListener("click", function () {
       element.classList.replace("setbut", "setbutact");
     } else {
       element.classList.replace("setbutact", "setbut");
+    }
+  });
+});
+
+//login button
+document.getElementById("logbutt").addEventListener("click", function () {
+  document.querySelectorAll(".logbut, .logbutact").forEach((element) => {
+    if (element.classList.contains("logbut")) {
+      element.classList.replace("logbut", "logbutact");
+    } else {
+      element.classList.replace("logbutact", "logbut");
+    }
+  });
+});
+
+//Sign in Button
+document.getElementById("sigbutt").addEventListener("click", function () {
+  document.querySelectorAll(".sigbut, .sigbutact").forEach((element) => {
+    if (element.classList.contains("sigbut")) {
+      element.classList.replace("sigbut", "sigbutact");
+    } else {
+      element.classList.replace("sigbutact", "sigbut");
     }
   });
 });
