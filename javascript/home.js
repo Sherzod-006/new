256f69fb65f8f2e03f177278683f51afa8027d31
Sherzod Sobirov <sherzodsobirovv1@gmail.com>
2025-05-17 11:10:01 +0500

    updated navigation bar and added a new section to the home page

--- javascript/home.js
+++ javascript/home.js
@@ -8,11 +8,26 @@ document
         document
           .querySelector(".fa-moon")
           .classList.replace("fa-moon", "fa-sun");
+
+        document.querySelector(".white").classList.replace("white", "black");
       } else {
         element.classList.replace("dark", "light");
         document
           .querySelector(".fa-sun")
           .classList.replace("fa-sun", "fa-moon");
+
+        document.querySelector(".black").classList.replace("black", "white");
       }
     });
   });
+
+// Settings Button
+document.getElementById("setbutt").addEventListener("click", function () {
+  document.querySelectorAll(".setbut, .setbutact").forEach((element) => {
+    if (element.classList.contains("setbut")) {
+      element.classList.replace("setbut", "setbutact");
+    } else {
+      element.classList.replace("setbutact", "setbut");
+    }
+  });
+});
